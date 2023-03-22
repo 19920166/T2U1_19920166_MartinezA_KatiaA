344e433bd9b01d181da8a84d8546ce00943dbe1b
Katia <L19920166@voaxaca.tecnm.mx>
2023-03-22 09:41:57 -0600

    .

---
+++ src/main/kotlin/Builder/Contact.kt
@@ -0,0 +1,6 @@
+package Builder
+
+data class Contact(val name: String?,
+                   val phone: Phone?,
+                   val address: Address?
+                )
---
+++ src/main/kotlin/Builder/Phone.kt
@@ -0,0 +1,6 @@
+package Builder
+
+data class Phone(
+    val phoneNumber: String?,
+    val ext: String?,
+    val phoneType: String?)
